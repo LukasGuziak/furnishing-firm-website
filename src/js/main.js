cd39e6d34a2b3188e21e6787b649c78c8d4e3f2f
Lukasz Guziak <guziak.luk@gmail.com>
2022-08-31 17:49:32 +0200

    navigation section update

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-document.body.style.background = 'grey';
+const navBtn = document.querySelector('#menu-btn');
+const navBar = document.querySelector('.navigation__navbar');
+
+console.log(navBar);
+console.log(navBtn);
+
+const navMobile = () => {
+	navBar.classList.toggle('active');
+};
+
+navBtn.addEventListener('click', navMobile);
+navBar.addEventListener('click',navMobile)
