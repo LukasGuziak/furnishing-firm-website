fcaac634f05e3a8271daab2bc9828baa9b2ca599
Lukasz Guziak <guziak.luk@gmail.com>
2022-09-23 17:30:58 +0200

    add thankyou.html site

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
 const navBtn = document.querySelector('#menu-btn');
 const navBar = document.querySelector('.navigation__navbar');
+const footerYear = document.querySelector('.footer__year');
 
 const navMobile = () => {
 	navBar.classList.toggle('active');
 };
+
+const hendleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+
 
 // !-- Initialize Swiper
 var swiper = new Swiper('.mySwiper', {
@@ -15,6 +23,8 @@ var swiper = new Swiper('.mySwiper', {
 	},
 });
 
+
+hendleCurrentYear();
 navBtn.addEventListener('click', navMobile);
 navBar.addEventListener('click', navMobile);
 
